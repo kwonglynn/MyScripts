70889ddd338bc55d4c4d76818e7e7a49648f1e57
Guanglin <kwonglynn@126.com>
2018-06-13 15:53:09 +0200

    restraint

--- Academic/Gromacs/PotentialScaledMD/process_restraints_alpha7.py
+++ Academic/Gromacs/PotentialScaledMD/process_restraints_alpha7.py
@@ -20,10 +20,10 @@ backbone2 = chain2.select_atoms("backbone").ids
 free2 = chain2.select_atoms("backbone and (byres (around 6 resname LIG))").ids
 
 print ("Free backbone atoms in the first chain:")
-for id in free1: print (id,)
+for id in free1: print ("%d " % id)
 print ("\n\n")
 print ("Free backbone atoms in the second chain:")
-for id in free2: print (id,)
+for id in free2: print ("%d " % id)
 
 title = '''\
 [ position_restraints ]
