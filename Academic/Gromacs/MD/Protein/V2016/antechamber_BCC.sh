20b97f6ce353117050af7d65ffd71a0b17fd0677
Guanglin <kwonglynn@126.com>
2018-06-13 17:41:39 +0200

    preprocess

--- Academic/Gromacs/MD/Protein/V2016/antechamber_BCC.sh
+++ Academic/Gromacs/MD/Protein/V2016/antechamber_BCC.sh
@@ -3,7 +3,7 @@ module load amber/16-nsc1
 antechamber -fi pdb -i LIG.pdb -fo mol2 -o LIG_bcc_gaff.mol2 -c bcc -at gaff -rn LIG 
 parmchk -f mol2 -i LIG_bcc_gaff.mol2 -o LIG.frcmod
 # Process with tleap
-tleap -s -f leap_gaff.in
+tleap -s -f leap_gaff_BCC.in
 acpype -p LIG.prmtop -x LIG.inpcrd
 sed -e "1,\$s/1  LIG/1LIG  /g" LIG_GMX.gro > LIG_MD.gro
 cp LIG_GMX.top LIG_MD.gro ..
