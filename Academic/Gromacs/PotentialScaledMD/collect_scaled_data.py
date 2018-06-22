74614970c899972c751d6bdbedf19cd2de99d6db
Guanglin <kwonglynn@126.com>
2018-06-22 11:34:12 +0200

    scaled

--- Academic/Gromacs/PotentialScaledMD/collect_scaled_data.py
+++ Academic/Gromacs/PotentialScaledMD/collect_scaled_data.py
@@ -6,12 +6,14 @@ with open('MDAnalysis_path.dat', 'r') as fi_path:
         path = path.strip()
         with open(path, 'r') as fi_data:
             for data in fi_data:
-                terms = data.split()
-                time = float(terms[0])
-                dist = float(terms[3])
-                if dist >= 2.0:
-                    fo.write("{.2f}\n".format(time))
-                    
+                if not data.startswith('#'):
+                    terms = data.split()
+                    time = float(terms[0])
+                    dist = float(terms[3])
+                    if dist >= 20:
+                        fo.write("{:.2f}\n".format(time))
+                        break
+                        
 fo.close()
                 
     
