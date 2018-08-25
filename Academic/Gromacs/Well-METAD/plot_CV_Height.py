6e2826494a2deeb6e291bd57a898a7d7014a0db3
Guanglin <kwonglynn@126.com>
2018-08-25 19:19:27 +0200

    metad

--- Academic/Gromacs/Well-METAD/plot_CV_Height.py
+++ Academic/Gromacs/Well-METAD/plot_CV_Height.py
@@ -7,19 +7,19 @@ data = pd.read_csv('HILLS', \
                    comment='#', header=None, \
                    names=['time', 'dist', 'dihed', 'sigma_dist', 'sigma_dihed', 'height', 'biasf'])
 
-time = np.array(data['time'])
-dist = np.array(data['dist'])
-height = np.array(data['height'])
+time = np.array(data['time']) / 1000
+dist = np.array(data['dist']) * 10
+height = np.array(data['height']) / 4.184
 
 fig1, ax1 = plt.subplots()
 
 ax1.plot(time, dist, 'bo')
-ax1.set(xlabel='Time (ns)', ylabel='distance (ns)')
+ax1.set(xlabel='Time (ns)', ylabel=r'$distance\ (\AA)$')
 
 fig1.savefig('Metad_distance.png')
 
 fig2, ax2 = plt.subplots()
 ax2.plot(time, height, 'bo')
-ax2.set(xlabel='Time (ns)', ylabel='height (kJ/mol)')
+ax2.set(xlabel='Time (ns)', ylabel='height (kcal/mol)')
 
 fig2.savefig('Metad_HILLS.png')
