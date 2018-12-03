7240078a4d74707f14dca986131573903b4a9ff2
Guanglin <kwonglynn@126.com>
2018-12-03 11:40:10 +0100

    AMBER

--- Academic/Amber/a-Syn-WC58a/process_complex_for_amber.py
+++ Academic/Amber/a-Syn-WC58a/process_complex_for_amber.py
@@ -5,27 +5,34 @@ state_name = glob.glob('state*frame*.pdb')[0]
 u = mda.Universe(state_name)
 
 prot_noH = u.select_atoms('protein and not name H*')
-prot_noH.write('protein_noH.pdb')
+prot_noH.write('protein_GMX_noH.pdb')
 
 LIG = u.select_atoms('resname LIG')
 LIG.write('LIG.pdb')
 
-fo = open('complex.pdb', 'w')
+fo_p = open('protein.pdb', 'w')
+fo_c = open('complex.pdb', 'w')
+
 with open('LIG.pdb', 'r') as fi:
     for line in fi:
         if line.startswith('ATOM'):
-            fo.write(line)
-    fo.write("TER\n")
+            fo_c.write(line)
+fo_c.write("TER\n")
 
-with open('protein_noH.pdb', 'r') as fi:
+with open('protein_GMX_noH.pdb', 'r') as fi:
     for line in fi:
         if line.startswith('ATOM'):
             if 'CD  ILE' in line:
                 line = line.replace('CD  ILE', 'CD1 ILE')
-            fo.write(line)
+            fo_p.write(line)
+            fo_c.write(line)
 
             if 'CH3 NME' in line:
-                fo.write("TER\n")
-    fo.write("END\n")
+                fo_p.write("TER\n")
+                fo_c.write("TER\n")
 
-fo.close()
+fo_p.write("END\n")
+fo_c.write("END\n")
+
+fo_p.close()
+fo_c.close()
--- Academic/Amber/a-Syn-WC58a/write_decompose_residues.py
+++ Academic/Amber/a-Syn-WC58a/write_decompose_residues.py
@@ -7,6 +7,7 @@ close_resid = u.select_atoms('name CA and same residue as (around 10 resname LIG
 residues = list(close_resid.resids)
 
 decom_resid = ','.join(map(str,residues))
+decom_resid = '1,' + decom_resid	#Add the resid of the ligand to the list, which is needed by the algorithm
 
 fo = open('06_MMPBSA.in', 'w')
 with open('06_MMPBSA_raw.in', 'r') as fi:
