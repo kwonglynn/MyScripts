9d96c948f4e28c2a38eed4b864b862c5099cb118
Guanglin <kwonglynn@126.com>
2019-04-30 17:17:18 +0200

    scrapy

---
+++ Academic/Python/scrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 30 15:59:32 2019
+
+@author: Guanglin Kuang
+"""
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
